aa8112dfdb0c406af9b0273ba447532e8c2bb87c
codelicious <franzkenny26995@gmail.com>
2019-05-20 21:36:25 +0700

    Add Currency Helper

---
+++ WebApplication1/Helper/Currency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Helper
+{
+    public static class Currency
+    {
+        public static string ToRupiah(this int angka)
+        {
+            return string.Format(CultureInfo.CreateSpecificCulture("id-id"), "Rp. {0:N}", angka);
+        }
+
+        
+        public static string ToRupiah(this decimal angka)
+        {
+            return string.Format(CultureInfo.CreateSpecificCulture("id-id"), "Rp. {0:N}", angka);
+        }
+
+        public static int ToAngka(this string rupiah)
+        {
+            return int.Parse(Regex.Replace(rupiah, @",.*|\D", ""));
+        }
+    }
+}
